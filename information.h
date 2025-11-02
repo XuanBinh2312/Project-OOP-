cffbf7514c882b9b32022fa623bb422cf64001a2
XuanBinh2312 <xuanbinhnguyen231206@gmail.com>
2025-11-02 23:19:46 +0700

    Update information.h

--- information.h
+++ information.h
@@ -1,10 +1,15 @@
 #pragma once
 #inlcude <iostream>
 #include <string>
+#include "Date.h"
 using namespace std;
 
 class Information {
 private: 
   string name;
+  Date birth;
+  string address;
+  string phoneNum;
+  
   
 };
