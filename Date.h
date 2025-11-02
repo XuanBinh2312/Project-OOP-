d5c7d54feefbe8decb4e226a3c11249306cfc0c7
XuanBinh2312 <xuanbinhnguyen231206@gmail.com>
2025-11-02 22:26:21 +0700

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,1 +1,41 @@
+#pragma once 
+#include <iostream>
+#include <cmath>
+#include <string.h>
+using namespace std;
 
+
+class Date {
+    private:
+    int day, month, year;
+    int day_in_month();
+    public:
+    Date();
+    Date(int d, int m, int y);
+    ~Date();
+
+    int getD() const {return day;}
+    int getM() const {return month;}
+    int getY() const {return year;}
+    
+    //Qua tai toan tu < la member cua lop >
+    bool operator < ( const Date& d);
+
+    //Qua tai toan tu < la non-member >
+    friend bool operator< ( const Date& d1 ,const Date& d2);
+    friend bool operator== ( const Date& d1 ,const Date& d2);
+    friend bool operator> ( const Date& d1 ,const Date& d2);
+
+
+    //Qua tai toan tu xuat nhap
+    friend ostream& operator<< (ostream& out, Date& d);
+    friend istream& operator>> (istream& in, Date& d);
+
+    void setDate(int newDate) const;
+
+    void input();
+    void display();
+
+    bool checkDate();
+
+};
